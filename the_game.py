4c51e0e39484599b5ee9fca7bb9a335903edfdf3
Matthew D'Angelo <matt.dangelo13@gmail.com>
2023-01-04 17:12:39 -0500

    Use key up/down and update tile selection logic

--- the_game.py
+++ the_game.py
@@ -7,28 +7,49 @@ pygame.init()
 w = 320
 h = 240
 surface = pygame.display.set_mode((w, h))
-color = (255, 0, 0)
+p_color = (255, 0, 0)
+ground_colors = [(0, 100, 0), (111, 78, 55), (255, 165, 0), (255, 140, 0)]
+gc_index = 0
+# grass_color = (0, 100, 0)
+# dirt_color = (111, 78, 55)
+# planted_color = (255, 165, 0)
+# grown_color = (255, 140, 0)
 clock = pygame.time.Clock()
-background_colors = ["green", "purple", "blue", "yellow", "orange"]
-background_index = 0
+tx = 50
+ty = 50
 px = 30
 py = 30
-
-surface.fill(background_colors[background_index])
-
-pygame.draw.rect(surface, color, pygame.Rect(px, py, 60, 60))
+p_size = 30
+movement_speed = 5
+surface.fill("green")
+pygame.draw.rect(surface, ground_colors[gc_index], pygame.Rect(tx, ty, w - (tx * 2), h - (ty * 2)))
+pygame.draw.rect(surface, p_color, pygame.Rect(px, py, p_size, p_size))
 pygame.display.flip()
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
+				py -= movement_speed
+			elif event.key == pygame.K_a:
+				px -= movement_speed
+			elif event.key == pygame.K_s:
+				py += movement_speed
+			elif event.key == pygame.K_d:
+				px += movement_speed				
+			elif event.key == pygame.K_SPACE:	
+				if px > tx and px < w - tx - p_size and py > ty and py < h - ty - p_size:
+					gc_index += 1
+	
+	if px > tx and px < w - tx - p_size and py > ty and py < h - ty - p_size:
+		p_color = (255, 255, 255)
+	else:
+		p_color = (255, 0, 0)	
 
-	background_index = (background_index + 1) % len(background_colors)
-	px += 10
-	px = min(px, w - 30 - 60)
-
-	surface.fill(background_colors[background_index])
-	pygame.draw.rect(surface, color, pygame.Rect(px, py, 60, 60))
+	surface.fill("green")
+	pygame.draw.rect(surface, ground_colors[gc_index], pygame.Rect(tx, ty, w - (tx * 2), h - (ty * 2)))
+	pygame.draw.rect(surface, p_color, pygame.Rect(px, py, p_size, p_size))
 	pygame.display.flip()
-	clock.tick(5)
+	clock.tick(60)
